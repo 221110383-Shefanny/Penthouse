8772fe160c446fbb86a38e7c808f840605351d5d
221110383-Shefanny <221110383@students.mikroskil.ac.id>
2024-11-15 17:52:38 +0700

    ....

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -32,7 +32,7 @@ class _LoginState extends State<Login> {
     });
 
     try {
-      // Cek apakah email cocok dengan data di Firestore
+      // Cek apakah email dan password cocok dengan data di Firestore
       var userSnapshot = await FirebaseFirestore.instance
           .collection('employees')
           .where('email', isEqualTo: _emailController.text.trim())
@@ -42,56 +42,65 @@ class _LoginState extends State<Login> {
         throw Exception('Akun tidak ditemukan');
       }
 
-      // Ambil data pengguna dari Firestore
-      var userData = userSnapshot.docs.first.data();
-      String storedPassword = userData['password']; // Password di Firestore
-      String name = userData['name']; // Ambil name dari Firestore
-      String position = userData['position']; // Ambil position dari Firestore
-
+      // Verifikasi password
+      String storedPassword = userSnapshot
+          .docs.first['password']; // Asumsi password disimpan di Firestore
       if (_passwordController.text.trim() != storedPassword) {
         throw Exception('Password salah');
       }
 
       // Proses login dengan Firebase Authentication
-      await _auth.signInWithEmailAndPassword(
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
-
-      // // Simpan data ke SharedPreferences
-      // SharedPreferences prefs = await SharedPreferences.getInstance();
-      // // await prefs.setString('userName', name);
-      // // await prefs.setString('userRole', position);
-      // await prefs.setBool('isLoggedIn', true);
 
       // Navigasi ke halaman home jika login berhasil
       if (mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => HomePage(userName: name, role: position),
+            builder: (context) => HomePage(userName: "Supervisor"),
           ),
         );
       }
 
-      // Log aktivitas login ke Firebase Analytics dan Firestore
+// Event untuk menandai pengguna aktif
+      await _analytics.logEvent(
+        name: 'active_user',
+        parameters: {
+          'email': _emailController.text.trim(),
+        },
+      );
+
+      // Log aktivitas login ke Firebase Analytics
       await _analytics.logEvent(
         name: 'login_event',
         parameters: {
           'email': _emailController.text.trim(),
-          'name': name,
         },
       );
+
+      // Simpan aktivitas login ke Firestore
       await FirebaseFirestore.instance.collection('logs').add({
         'email': _emailController.text.trim(),
-        'username': name,
         'status': 'login',
         'timestamp': FieldValue.serverTimestamp(),
       });
-    } catch (e) {
-      setState(() {
-        isLoading = false;
+
+      // Simpan aktivitas login ke Firebase Realtime Database
+      DatabaseReference ref = FirebaseDatabase.instance.ref("logs");
+      await ref.push().set({
+        'email': _emailController.text.trim(),
+        'status': 'login',
+        'timestamp': ServerValue.timestamp,
       });
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
 
       // Menampilkan error jika login gagal
       showDialog(
